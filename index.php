ff0229c333d35c3b5aacb4c341bd1e8ce3d67813
Kilián Marcell <kilian.marcell.andras@gmail.com>
2021-11-10 12:01:58 +0100

    Hello word kiírása

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
 
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\Factory\AppFactory;
+
 require 'vendor/autoload.php';
+
+$app = AppFactory::create();
+$app->setBasePath('/Interface');
+
+$app->get('/', function (ServerRequestInterface $request,
+    ResponseInterface $response) {
+        $response->getBody()->write('Hello world!');
+        return $response;
+    });
+
+$app->run();
